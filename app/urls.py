7f7af7c42fb14198e6d9264e903568ee12eabe1b
Marceli <nowak14marceli@gmail.com>
2025-12-08 21:29:10 +0100

    Style 1
    +dodanie pliku ze stylami wraz z zagnieżdżaniem
    +nowy wygląd formularzy logowania i rejestracji

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.game_list, name='game_list'),
+    path('', views.game_list, name='mainpage'),
     path('game/<int:game_id>/', views.game_detail, name='game_detail'),
     path('review/<int:review_id>/', views.review_detail, name='review_detail'),
 
     # logowanie / wylogowanie / rejestracja
     path('login/', auth_views.LoginView.as_view(template_name='reviews/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='game_list'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='mainpage'), name='logout'),
     path('register/', views.register, name='register'),
 ]
